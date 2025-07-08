a177cd3be52558aa02cf53fd22d83e511822e4e5
tm-pham <thi.mui.pham@posteo.de>
2025-07-08 10:54:45 -0400

    Added code for figure 3AC

---
+++ code/figure3/lsarp_figure3A_ENTFAES.R
@@ -0,0 +1,100 @@
+# ============================================================================ #
+# Project: LSARP
+# Author: Thi Mui Pham, tmpham@hsph.harvard.edu
+# Title: Figure 3A
+# Organism: Enterococcus faecalis
+# ---------------------------------------------------------------------------- #
+# Phenotype resistance profile for E faecalis 
+# ============================================================================ #
+remove(list=ls())
+# Set working directory here
+setwd("/Users/tm-pham/academia/hsph/LSARP_amr_genomics/")
+
+# Load packges
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(lubridate)
+library(cowplot)
+library(ggh4x)
+library(gridExtra)
+
+# Load functions and templates
+source("code/functions_templates/plotting_template.R")
+
+################################################################################
+# Figure 3A
+ab_colors <- c("#1B9E77", "#D95F02" ,"#E7298A")
+strip_text_size <- 34
+axis_text_x <- 24
+axis_title_y <- 28
+
+################################################################################
+# COMMUNITY-ONSET
+(plot1.1 <- ggplot(df_mlst_ab_COI_yr %>% 
+                     filter(ab_name%in%c("R-S-S", "R-R-S", "R-R-R")) %>% 
+                     filter(cluster_name%in%c("1 (ST 179)", "3 (ST 16)", "2 (ST 40)","6 (ST 64)", "4 (ST 778/103)", "5 (ST 6)")),
+                   aes(x=ymd(YEAR, truncated=2), y=inc, fill=ab_name, color = ab_name)) + 
+    facet_grid(rows = vars(HOSPITAL_ONSET_48H), cols=vars(cluster_name)) + 
+    geom_line(linetype = "dashed", linewidth = 1.0) +
+    stat_smooth(method = "lm", formula = y ~ x , size = 3, expand = c(0, 0)) +
+    geom_point(shape = 21, color = "black", size = 4.5, stroke = 2) + 
+    labs(y="Number of index isolates\n(per 100,000 Calgary residents)", 
+         fill = "", 
+         color = "") + 
+   scale_x_date(date_labels = "%Y", breaks = seq.Date(from = as.Date("2006-01-01"), 
+                                                      to = as.Date("2022-12-31"), by = "4 years")) + 
+    scale_y_continuous(limits = c(-0.2, 1.5), expand = c(0, 0)) +
+    coord_cartesian(ylim = c(0, 1.5)) + 
+    scale_fill_manual(values = ab_colors) +
+    scale_color_manual(values = ab_colors) +
+    theme_template_white()+ 
+    theme(legend.position = "top",
+          legend.direction = "vertical", 
+          legend.text = element_text(size = strip_text_size),
+          legend.title = element_blank(),
+          panel.spacing = unit(1.2, "cm"),
+          strip.text.x = element_text(size = strip_text_size, face = "bold"),
+          strip.text.y = element_text(size = strip_text_size-1, face = "bold"),
+          axis.title.x = element_blank(), 
+          axis.title.y = element_text(size = axis_title_y),
+          axis.text.y = element_text(size = axis_text_x),
+          axis.text.x = element_text(size = axis_text_x, angle = 0)))
+
+################################################################################
+# HOSPITAL-ONSET
+(plot1.2 <- ggplot(df_mlst_ab_HOI_yr %>% 
+                     filter(ab_name%in%c("R-S-S", "R-R-S", "R-R-R")) %>% 
+                     filter(cluster_name%in%c("1 (ST 179)", "3 (ST 16)", "2 (ST 40)", "4 (ST 778/103)", "6 (ST 64)", "5 (ST 6)")), 
+                   aes(x=ymd(YEAR, truncated=2), y=inc, fill=ab_name, color = ab_name)) + 
+    facet_grid(rows = vars(HOSPITAL_ONSET_48H), cols=vars(cluster_name)) + 
+    geom_line(linetype = "dashed", linewidth = 1.0) +
+    stat_smooth(method = "lm", formula = y ~ x , size = 3, span = 1, fullrange=T, level = 0.95) +
+    # geom_smooth(span = 2) +
+    geom_point(shape = 21, color = "black", size = 4.5, stroke = 2) + 
+    labs(y="Number of index isolates\n(per 100,000 patient days)", 
+         fill = "", 
+         color = "") + 
+   scale_x_date(date_labels = "%Y", breaks = seq.Date(from = as.Date("2006-01-01"), 
+                                                      to = as.Date("2022-12-31"), by = "4 years")) + 
+    scale_y_continuous(limits = c(-0.4, 1.5), expand = c(0, 0)) +
+    coord_cartesian(ylim = c(0, 1.5)) + 
+    scale_fill_manual(values = ab_colors) +
+    scale_color_manual(values = ab_colors) +
+    theme_template_white()+ 
+    theme(legend.position = "none", 
+          panel.spacing = unit(1.2, "cm"),
+          strip.text.x = element_text(size = strip_text_size, face = "bold"),
+          strip.text.y = element_text(size = strip_text_size-1, face = "bold"),
+          axis.title.x = element_blank(), 
+          axis.title.y = element_text(size = axis_title_y),
+          axis.text.y = element_text(size = axis_text_x),
+          axis.text.x = element_text(size = axis_text_x, angle = 0)))
+
+################################################################################
+# Combined plot
+(plot <- grid.arrange(plot1.1, plot1.2, ncol = 1, heights = c(1, 0.85)))
+ggsave(plot, file = "figures/figure3/lsarp_figure3A.pdf", 
+       width = 36, height = 13)
+ggsave(plot, file = "figures/figure3/lsarp_figure3A.png",
+       width = 36, height = 13, dpi = 300)
---
+++ code/figure3/lsarp_figure3C_ENTFAEM.R
@@ -0,0 +1,121 @@
+# ============================================================================ #
+# Project: LSARP
+# Author: Thi Mui Pham, tmpham@hsph.harvard.edu
+# Organism: Enterococcus faecium
+# Title: Figure 3C, ST117 plot
+# ============================================================================ #
+remove(list=ls())
+# Set working directory here
+setwd("/Users/tm-pham/academia/hsph/LSARP_amr_genomics/")
+
+# Load package 
+library(gridExtra)
+library(grid)
+library(ggplot2)
+library(lattice)
+library(dplyr)
+library(lubridate)
+
+# Load functions and templates
+source("code/functions_templates/plotting_template.R")
+
+################################################################################
+# Variables
+axis_text_x <- 26
+strip_text_size <- 30
+
+################################################################################
+# Plot
+################################################################################
+(st117_yr_plot <- ggplot(df_st117_HOI_overall_yr %>% filter(HOSPITAL_ONSET_48H=="Hospital-onset"), 
+                         aes(x=ymd(YEAR, truncated=2), y = inc)) +
+    facet_wrap(.~cluster_name) + 
+    geom_line(linetype="dashed", color="darkgrey") + 
+    geom_point(size=7.5) + 
+    geom_smooth(linewidth=4, color = "black") + 
+    labs(y="Number of 30-day index isolates\n(per 100,000 patient days)") + 
+    scale_y_continuous(limits= c(0, NA)) +
+    scale_x_date(date_labels="%Y", breaks=seq(as.Date("2006-01-01", format = "%Y-%m-%d"), 
+                                              as.Date("2022-12-31", format = "%Y-%m-%d"), 
+                                              by = "year")) + 
+    scale_fill_scico_d() + 
+    theme_template() + 
+    theme(axis.text.x = element_text(size=24), 
+          axis.title.y = element_text(size=28, face="plain"),
+          strip.text = element_text(size=strip_text_size),
+          legend.position = "none", 
+          plot.background = element_rect(fill = "transparent", colour = NA)))
+
+################################################################################
+# Overall HOI plot with segmented Poisson regression
+################################################################################
+dataset <- df_st117_HOI_overall_yr %>% 
+  filter(HOSPITAL_ONSET_48H=="Hospital-onset") %>% 
+  mutate(ST_name = "E8328 (ST117)")
+
+model<- glm(n ~ YEAR + offset(log(total_ptdays)), 
+            data = dataset, 
+            family = poisson())
+
+davies.test(model, seg.Z = ~YEAR, k = 10)
+(seg_model <- segmented::segmented(model, npsi = 1))
+
+# Extract the breakpoints
+(breaks <- seg_model$psi[, 2])
+
+(plot1 <- ggplot(data = dataset, aes(x = ymd(YEAR, truncated=2), y = inc)) +
+    facet_wrap(.~cluster_name) + 
+    geom_point(color = "black", shape = 21, stroke = 1.5, size = 4, fill = "white") +
+    geom_smooth(data = dataset %>% filter(YEAR<breaks), color = "black", size = 2, 
+                method = "glm", method.args = list(family = "poisson")) +
+    geom_smooth(data = dataset %>% filter(YEAR>=breaks), color = "black", size = 2, 
+                method = "glm", method.args = list(family = "poisson")) +
+    scale_y_continuous(limits = c(0, NA)) + 
+    scale_x_date(date_labels = "%Y", breaks = seq.Date(from = as.Date("2006-01-01"), 
+                                                       to = as.Date("2022-12-31"), by = "2 years"), 
+                 limits = c(as.Date("2006-01-01"), as.Date("2022-12-31"))) + 
+    labs(y="Number of index isolates\n(per 100,000 patient days)") + 
+    theme_template_white() + 
+    theme(
+          axis.title.x = element_blank(), 
+          axis.title.y = element_text(size=26, face="plain"),
+          axis.text.x = element_text(size=axis_text_x, face="plain"),
+          axis.text.y = element_text(size=22, face="plain"),
+          strip.text = element_text(size = strip_text_size, face = "bold.italic")))
+
+################################################################################
+# Plot of ST117 by antibiogram
+(plot2 <- ggplot(df_st117_HOI_yr %>%
+                   filter(HOSPITAL_ONSET_48H=="Hospital-onset", 
+                          ab_name %in% c("S-S-S", "R-S-S", "S-R-S", 
+                                         "S-R-R", "R-R-S", "R-R-R")) %>% 
+                   mutate(ab_name = factor(ab_name, levels =  c("S-S-S", "R-S-S", "S-R-S", 
+                                                                "S-R-R", "R-R-S", "R-R-R"))), 
+                 aes(x = ymd(YEAR, truncated = 2), 
+                     y = inc, fill=ab_name)) + 
+   facet_wrap(.~ab_name) + 
+   geom_bar(stat="identity", color = "black", size = 1.2) +
+   labs(y="") + 
+   scale_y_continuous(limits= c(0, NA)) +
+   scale_fill_manual(values= c("#1B9E77", "#E7298A", "#004B40", "#D55E00", "#EFC000FF", "#660066")) +
+   scale_x_date(date_labels="%Y", breaks=seq(as.Date("2006-01-01", format = "%Y-%m-%d"), 
+                                             as.Date("2022-12-31", format = "%Y-%m-%d"), 
+                                             by = "4 years")) + 
+   theme_template_white() + 
+   theme(axis.text.x = element_text(size=18), 
+         panel.spacing = unit(1, "cm"),
+         strip.text = element_text(size=strip_text_size, face = "bold"),
+         axis.title.y = element_text(size=22, face="plain"),
+         axis.text.y = element_text(size=20, face="plain"),
+         axis.title.x = element_blank(),
+         legend.position = "none", 
+         plot.background = element_rect(fill = "transparent", colour = NA)))
+
+################################################################################
+# Combined plot
+lay <- rbind(c(1,1,1,1,1,2,2,2,2), c(1,1,1,1,1,2,2,2,2))
+plot <- grid.arrange(plot1, plot2,
+                     layout_matrix = lay)
+ggsave(plot, file = "figures/figure3/lsarp_figure3C.pdf", 
+       width = 24, height=7.5, bg = "transparent")
+
